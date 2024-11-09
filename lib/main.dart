053f87d14735fc13a5cb85b808facca9b9a7e04d
Wi42 <101128608+Wii42@users.noreply.github.com>
2024-11-09 22:28:44 +0100

    reordered main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,38 +7,6 @@ import 'package:kgl_time/persistent_storage/persistent_storage_service.dart';
 import 'data_model/work_category.dart';
 import 'kgl_time_app.dart';
 
-List<WorkCategory> mockWorkCategories = [
-  WorkCategory('Telefonanruf'),
-  WorkCategory('Kategorie 2'),
-  WorkCategory('Kategorie 3'),
-  WorkCategory('Kategorie 4'),
-  WorkCategory('Kategorie 5'),
-];
-
-List<WorkEntry> mockWorkEntries = [
-  WorkEntry(
-    workDurationInSeconds: const Duration(hours: 1).inSeconds,
-    date: DateTime.now(),
-    description: 'Test',
-    categories: [mockWorkCategories[0].toEmbedded()],
-  ),
-  WorkEntry(
-    workDurationInSeconds: const Duration(minutes: 30).inSeconds,
-    date: DateTime.now().subtract(const Duration(days: 1, hours: 12)),
-    description: 'Test2',
-    categories: [mockWorkCategories[1].toEmbedded()],
-  ),
-  WorkEntry(
-    workDurationInSeconds: const Duration(hours: 1, minutes: 30).inSeconds,
-    date: DateTime.now().subtract(const Duration(days: 7)),
-    description: 'old entry',
-    categories: [
-      mockWorkCategories[3].toEmbedded(),
-      mockWorkCategories[4].toEmbedded()
-    ],
-  ),
-];
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await PersistentStorageService.initializeImplementation(
@@ -66,3 +34,35 @@ void main() async {
     initialKeyValueStorage: initialKeyValueStorage,
   ));
 }
+
+List<WorkCategory> get mockWorkCategories => [
+  WorkCategory('Telefonanruf'),
+  WorkCategory('Kategorie 2'),
+  WorkCategory('Kategorie 3'),
+  WorkCategory('Kategorie 4'),
+  WorkCategory('Kategorie 5'),
+];
+
+List<WorkEntry> get mockWorkEntries => [
+  WorkEntry(
+    workDurationInSeconds: const Duration(hours: 1).inSeconds,
+    date: DateTime.now(),
+    description: 'Test',
+    categories: [mockWorkCategories[0].toEmbedded()],
+  ),
+  WorkEntry(
+    workDurationInSeconds: const Duration(minutes: 30).inSeconds,
+    date: DateTime.now().subtract(const Duration(days: 1, hours: 12)),
+    description: 'Test2',
+    categories: [mockWorkCategories[1].toEmbedded()],
+  ),
+  WorkEntry(
+    workDurationInSeconds: const Duration(hours: 1, minutes: 30).inSeconds,
+    date: DateTime.now().subtract(const Duration(days: 7)),
+    description: 'old entry',
+    categories: [
+      mockWorkCategories[3].toEmbedded(),
+      mockWorkCategories[4].toEmbedded()
+    ],
+  ),
+];
