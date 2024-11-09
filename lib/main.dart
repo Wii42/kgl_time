2c5735df5cfa6c6ddf97aada3d6c9477e229e74c
Wi42 <101128608+Wii42@users.noreply.github.com>
2024-11-09 23:32:30 +0100

    set up loadTest, large number of entries

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:kgl_time/data_model/work_entry.dart';
 import 'package:kgl_time/persistent_storage/isar_persistent_storage.dart';
 import 'package:kgl_time/persistent_storage/persistent_storage_service.dart';
+import 'package:kgl_time/persistent_storage/temp_storage.dart';
 
 import 'data_model/work_category.dart';
 import 'kgl_time_app.dart';
 
+bool isLoadTest = false;
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await PersistentStorageService.initializeImplementation(
-      IsarPersistentStorage());
+      isLoadTest ? TempStorage() : IsarPersistentStorage());
   List<WorkEntry> initialEntries =
       await PersistentStorageService.instance.workEntries.loadEntries();
   List<WorkCategory> initialCategories =
@@ -29,40 +34,58 @@ void main() async {
       'de', '_'); // initialize the date formatting for German
   runApp(KglTimeApp(
     appTitle: 'KGL Time',
-    initialEntries: initialEntries,
+    initialEntries: isLoadTest ? loadTestEntries() : initialEntries,
     initialCategories: initialCategories,
     initialKeyValueStorage: initialKeyValueStorage,
   ));
 }
 
 List<WorkCategory> get mockWorkCategories => [
-  WorkCategory('Telefonanruf'),
-  WorkCategory('Kategorie 2'),
-  WorkCategory('Kategorie 3'),
-  WorkCategory('Kategorie 4'),
-  WorkCategory('Kategorie 5'),
-];
+      WorkCategory('Telefonanruf'),
+      WorkCategory('Kategorie 2'),
+      WorkCategory('Kategorie 3'),
+      WorkCategory('Kategorie 4'),
+      WorkCategory('Kategorie 5'),
+    ];
 
 List<WorkEntry> get mockWorkEntries => [
-  WorkEntry(
-    workDurationInSeconds: const Duration(hours: 1).inSeconds,
-    date: DateTime.now(),
-    description: 'Test',
-    categories: [mockWorkCategories[0].toEmbedded()],
-  ),
-  WorkEntry(
-    workDurationInSeconds: const Duration(minutes: 30).inSeconds,
-    date: DateTime.now().subtract(const Duration(days: 1, hours: 12)),
-    description: 'Test2',
-    categories: [mockWorkCategories[1].toEmbedded()],
-  ),
-  WorkEntry(
-    workDurationInSeconds: const Duration(hours: 1, minutes: 30).inSeconds,
-    date: DateTime.now().subtract(const Duration(days: 7)),
-    description: 'old entry',
-    categories: [
-      mockWorkCategories[3].toEmbedded(),
-      mockWorkCategories[4].toEmbedded()
-    ],
-  ),
-];
+      WorkEntry(
+        workDurationInSeconds: const Duration(hours: 1).inSeconds,
+        date: DateTime.now(),
+        description: 'Test',
+        categories: [mockWorkCategories[0].toEmbedded()],
+      ),
+      WorkEntry(
+        workDurationInSeconds: const Duration(minutes: 30).inSeconds,
+        date: DateTime.now().subtract(const Duration(days: 1, hours: 12)),
+        description: 'Test2',
+        categories: [mockWorkCategories[1].toEmbedded()],
+      ),
+      WorkEntry(
+        workDurationInSeconds: const Duration(hours: 1, minutes: 30).inSeconds,
+        date: DateTime.now().subtract(const Duration(days: 7)),
+        description: 'old entry',
+        categories: [
+          mockWorkCategories[3].toEmbedded(),
+          mockWorkCategories[4].toEmbedded()
+        ],
+      ),
+    ];
+
+List<WorkEntry> loadTestEntries() {
+  Random rand = Random(0);
+  return [for (int i = 0; i < 10000; i++) createRandomWorkEntry(rand)];
+}
+
+WorkEntry createRandomWorkEntry(Random rand) {
+  return WorkEntry.fromDuration(
+    duration: Duration(
+      minutes: rand.nextInt(180),
+      seconds: rand.nextInt(60),
+    ),
+    date: DateTime.now().subtract(Duration(days: rand.nextInt(3650))),
+    categories: (mockWorkCategories.map((e) => e.toEmbedded()).toList()
+      ..shuffle(rand))
+      .take(rand.nextInt(mockWorkCategories.length)).toList(),
+  );
+}
